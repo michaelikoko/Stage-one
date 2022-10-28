d3f5515f6f0ac45f414134f154e11e91d261f875
michael_ikoko <michaelikoko.o@gmail.com>
2022-10-28 05:15:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# STAGE ONE TASK FOR HNG9 INTERNSHIP
+Create an **(GET)** api endoint that returns the following  json response:
+{ "**slackUsername**": String, "**backend**": Boolean, "**age**": Integer, "**bio**": String }   
+- SlackUsername should be a **string** datatype and your slack username
+- Backend should be a **boolean** datatype
+- Age should be an  **integer** datatype
+- Bio(description about yourself) should be a **string** datatype
---
+++ main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# Create your views here.
+
+class MainView(APIView):
+    """The view for the only API endpoint that returns a JSON response containing an object."""
+    
+    def get(self, request):
+        print("hey")
+        return Response(
+            { 
+                "slackUsername": "michael_ikoko", 
+                "backend": True, 
+                "age": 19, 
+                "bio": "I am Michael Ikoko. I am a student and I am currently learing Backend web development." 
+            }
+        )
